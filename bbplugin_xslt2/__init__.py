5ada84537fede2aad8e597ea94b585c71d4e1bdc
Niels <niels.hoffmann@hotmail.com>
2026-05-11 22:14:23 +0200

    fixes

--- bbplugin_xslt2/__init__.py
+++ bbplugin_xslt2/__init__.py
@@ -1,27 +1,32 @@
 import logging
 from saxonche import PySaxonProcessor
+from ogc.bblocks.models import TransformMetadata, Transformer
+from typing import AnyStr
+
 
 # Configure logging to see Saxon errors in the BBlocks build logs
 logger = logging.getLogger(__name__)
 
+transform_types = ['xslt2']
+# Define supported mime types
+default_inputs = ['application/xml']
+default_outputs = ['application/xml']
 
-
-class SaxonXsltTransformer:
+class SaxonXsltTransformer(Transformer):
     """
     A Transformer plugin that supports XSLT 2.0 using the Saxon-HE 
     (SaxonChe) library.
     """
-    transform_types = ['xslt2']
-    # Define supported mime types
-    default_inputs = ['application/xml']
-    default_outputs = ['application/xml']
+    
    
+    def __init__(self):
+        super().__init__(['xslt2'], default_inputs, default_outputs)
 
-    def transform(self, metadata):
+    def do_transform(self, metadata: TransformMetadata) -> AnyStr | None:
         try:
             # SaxonChe requires a context manager to manage the C++ native resources
             with PySaxonProcessor(license=False) as proc:
-                xslt_proc = proc.new_xslt20_processor()
+                xslt_proc = proc.new_xslt30_processor()
                 
                 # Load the input XML
                 # Saxon handles encoding internally from the string or bytes provided
@@ -38,7 +43,7 @@ class SaxonXsltTransformer:
                 # Perform transformation
                 result_str = executable.transform_to_string(xdm_node=input_node)
                 
-                return str(result_str)
+                return str(result_str).decode('utf-8')
 
         except Exception as e:
             logger.error(f"Error during Saxon XSLT transformation: {str(e)}")
